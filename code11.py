a67f1335c7ca078d6e8f1c485183262ee97e2e3b
Miguel dos Santos Girardi <miguel-girardi@estudante.rs.gov.br>
2026-06-26 15:41:43 -0300

    Fix greeting message formatting in code11.py

--- code11.py
+++ code11.py
@@ -1,5 +1,5 @@
 nome=input("Qual o seu nome? ")
-print("Olá" + nome + "!")
+print("Olá, " + nome + "!")
 idade=int(input("Tua idade? "))
 
 print("você nasceu em " + str(2026-idade) + "ou em " + str(2025-idade) + "!")
