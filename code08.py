c00267900324f72ed47629c782b38e8cc1492063
Miguel dos Santos Girardi <miguel-girardi@estudante.rs.gov.br>
2026-06-26 15:16:47 -0300

    Add variable definitions and loop examples in code08.py

---
+++ code08.py
@@ -0,0 +1,16 @@
+# Variable definitions
+name = "Miguel"      # string
+count = 1           # number
+
+# While loop
+while count <= 3:
+    print("While loop count:", count)
+    count += 1
+
+# For loop
+for num in range(1, 6):
+    # If statement
+    if num % 2 == 0:
+        print(num, "is even")
+    else:
+        print(num, "is odd")
