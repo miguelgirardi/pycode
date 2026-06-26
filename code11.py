8f0a42ff1f98de42ba7ec2f8f91fe19f52213ad4
Miguel dos Santos Girardi <miguel-girardi@estudante.rs.gov.br>
2026-06-26 15:42:02 -0300

    Fix print statement syntax in code11.py

--- code11.py
+++ code11.py
@@ -2,4 +2,4 @@ nome=input("Qual o seu nome? ")
 print("Olá, " + nome + "!")
 idade=int(input("Tua idade? "))
 
-print("você nasceu em " + str(2026-idade) + "ou em " + str(2025-idade) + "!")
+print("você nasceu em " + str(2026-idade) + " ou em " + str(2025-idade) + "!")
